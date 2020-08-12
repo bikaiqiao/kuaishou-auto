61c30de055def009d0ea40517c6cf8b7958edda6
bikaiqiao <965233637@qq.com>
2020-08-12 16:24:15 +0800

    暂时弃用test文件。脚本入口在auto.js

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
+//暂时弃用
 //直接使用message对象里面的方法应该也是可以的
 const message1 = require('ClassMessage.js')
 //引入socket方法连接客户端和服务器
 const socket = require('Socket')
 let name = "testBot"
 let messageText = "ding"
-
-
-
 
 // message1.goToMessagePage()
 
